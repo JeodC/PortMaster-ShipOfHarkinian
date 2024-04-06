fb7bbec6114d809b98a1d1943440b734073c6b35
JeodC <47716344+JeodC@users.noreply.github.com>
2024-04-06 09:31:58 -0400

    Add presets and a config.ini file

--- Ship of Harkinian.sh
+++ Ship of Harkinian.sh
@@ -29,6 +29,51 @@ cd $GAMEDIR
 $ESUDO chmod 666 /dev/tty1
 $ESUDO chmod 666 /dev/uinput
 
+# Function to apply settings from preset.ini to shipofharkinian.json
+apply_settings() {
+    set -x  # Enable debugging output
+    # Check preset value and copy preset.json accordingly
+    preset_value=$(grep -Po 'preset=\K\w+' config.ini)
+    case $preset_value in
+        default)
+            cp "presets/default.json" "shipofharkinian.json"
+            ;;
+        vanillaplus)
+            cp "presets/vanillaplus.json" "shipofharkinian.json"
+            ;;
+        enhanced)
+            cp "presets/enhanced.json" "shipofharkinian.json"
+            ;;
+        *)
+            cp "presets/default.json" "shipofharkinian.json"
+            ;;
+    esac
+
+	# Define array of keys to extract
+	keys=("internalresolution" "interpolationfps" "authenticlogo" "disablelod" "disabledrawdistance" "disablekokiridrawdistance" "usecustomtextures")
+
+	# Iterate over keys and extract corresponding values from preset.ini
+	for key in "${keys[@]}"; do
+		value=$(grep -Po "${key}=\K[^;]+" config.ini)
+		declare "$key=$value"
+	done
+
+    # Modify shipofharkinian.json according to additional options
+    sed -i -E \
+        -e "s/gAltAssets\": [01]+,/gAltAssets\": $usecustomtextures,/" \
+        -e "s/gAuthenticLogo\": [01],/gAuthenticLogo\": $authenticlogo,/" \
+        -e "s/gDisableLOD\": [01]+,/gDisableLOD\": $disablelod,/" \
+        -e "s/gDisableDrawDistance\": [01]+,/gDisableDrawDistance\": $disabledrawdistance,/" \
+        -e "s/gDisableKokiriDrawDistance\": [01]+,/gDisableKokiriDrawDistance\": $disablekokiridrawdistance,/" \
+        -e "s/gInternalResolution\": [0-9]+(\.[0-9]+)?,/gInternalResolution\": $internalresolution,/" \
+        -e "s/gInterpolationFPS\": [0-9]+,/gInterpolationFPS\": $interpolationfps,/" \
+        shipofharkinian.json
+    set +x  # Disable debugging output
+}
+
+# Call the function
+apply_settings > settings.log 2>&1  # Redirect output to settings.log
+
 # Run the game
 echo "Loading, please wait... (might take a while!)" > /dev/tty0
 $GPTOKEYB "soh.elf" -c "opt.gptk" & 
