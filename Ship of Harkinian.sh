cf7c2cca4dd6a6aa14e5ced7554c9e8b6da969ad
JeodC <47716344+JeodC@users.noreply.github.com>
2024-07-17 09:32:39 -0400

    Update Ship of Harkinian.sh

--- Ship of Harkinian.sh
+++ Ship of Harkinian.sh
@@ -13,14 +13,15 @@ else
 fi
 
 source $controlfolder/control.txt
-source $controlfolder/device_info.txt
-
-[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
-
 get_controls
 
 # Set variables
 GAMEDIR="/$directory/ports/soh"
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+# Source Device Info
+source $controlfolder/device_info.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 
 # Exports
 export LD_LIBRARY_PATH="$GAMEDIR/libs:/usr/lib"
@@ -31,17 +32,29 @@ $ESUDO chmod 666 /dev/tty1
 
 cd $GAMEDIR
 
-# Remove soh generated logs and substitute our own
-rm -rf $GAMEDIR/logs/* && exec > >(tee "$GAMEDIR/logs/log.txt") 2>&1
+# Array of compatibility firmwares
+CFW_NAMES=("ArkOS" "ArkOS wuMMLe" "ArkOS AeUX" "knulli")
 
-# Copy the right build to the main folder
-if [ "$CFW_NAME" == 'ArkOS' ] || [ "$CFW_NAME" == 'ArkOS wuMMLe' ] || [ "$CFW_NAME" == "knulli" ]; then
-	cp -f "$GAMEDIR/bin/compatibility.elf" soh.elf
-	if [ "$(find "./mods" -name '*.otr')" ]; then
-		echo "WARNING: .OTR MODS FOUND! PERFORMANCE WILL BE LOW IF ENABLED!!" > /dev/tty0
-	fi
+# Function to check if a value is in an array
+contains() {
+    local value="$1"
+    shift
+    for item; do
+        if [ "$item" == "$value" ]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+# Check if the current CFW name is in the array
+if contains "$CFW_NAME" "${CFW_NAMES[@]}"; then
+    cp -f "$GAMEDIR/bin/compatibility.elf" soh.elf
+    if [ "$(find "./mods" -name '*.otr')" ]; then
+        echo "WARNING: .OTR MODS FOUND! PERFORMANCE WILL BE LOW IF ENABLED!!" > /dev/tty0
+    fi
 else
-	cp -f "$GAMEDIR/bin/performance.elf" soh.elf
+    cp -f "$GAMEDIR/bin/performance.elf" soh.elf
 fi
 
 # Run the game
