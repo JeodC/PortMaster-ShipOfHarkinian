e762b3a7de2fbb4f9b9175b3e766ee03477b536e
JeodC <47716344+JeodC@users.noreply.github.com>
2024-07-20 08:30:31 -0400

    Use CUR_TTY variable

--- Ship of Harkinian.sh
+++ Ship of Harkinian.sh
@@ -23,6 +23,12 @@ GAMEDIR="/$directory/ports/soh"
 source $controlfolder/device_info.txt
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 
+CUR_TTY="/dev/tty0"
+# Set current virtual screen
+if [ "$CFW_NAME" == "muOS" ]; then
+  /opt/muos/extra/muxlog & CUR_TTY="/tmp/muxlog_info"
+fi
+
 # Exports
 export LD_LIBRARY_PATH="$GAMEDIR/libs:/usr/lib"
 
@@ -51,7 +57,7 @@ contains() {
 if contains "$CFW_NAME" "${CFW_NAMES[@]}"; then
     cp -f "$GAMEDIR/bin/compatibility.elf" soh.elf
     if [ "$(find "./mods" -name '*.otr')" ]; then
-        echo "WARNING: .OTR MODS FOUND! PERFORMANCE WILL BE LOW IF ENABLED!!" > /dev/tty0
+        echo "WARNING: .OTR MODS FOUND! PERFORMANCE WILL BE LOW IF ENABLED!!" > $CUR_TTY
     fi
 else
     cp -f "$GAMEDIR/bin/performance.elf" soh.elf
@@ -60,13 +66,13 @@ fi
 # Check if we need to generate any otr files
 if [ ! -f "oot.otr" ] || [ ! -f "oot-mq.otr" ]; then
     if ls *.*64 1> /dev/null 2>&1; then
-        echo "We need to generate OTR files! Stand by..." > /dev/tty0
+        echo "We need to generate OTR files! Stand by..." > $CUR_TTY
         ./assets/extractor/otrgen.txt
     fi
 fi
 
 # Run the game
-echo "Loading, please wait... (might take a while!)" > /dev/tty0
+echo "Loading, please wait... (might take a while!)" > $CUR_TTY
 
 $GPTOKEYB "soh.elf" -c "soh.gptk" & 
 ./soh.elf
