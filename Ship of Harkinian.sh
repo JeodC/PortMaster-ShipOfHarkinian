267d29570118009fde51a99fc158b6a7dddb5ffb
Jeod <47716344+JeodC@users.noreply.github.com>
2024-08-27 13:51:58 -0400

    Update Ship of Harkinian.sh

--- Ship of Harkinian.sh
+++ Ship of Harkinian.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
 
@@ -15,25 +15,19 @@ fi
 source $controlfolder/control.txt
 get_controls
 
-# Set variables
-GAMEDIR="/$directory/ports/soh"
-> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
-
 # Source Device Info
 source $controlfolder/device_info.txt
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 
-# Set current virtual screen
-if [ "$CFW_NAME" == "muOS" ]; then
-  /opt/muos/extra/muxlog & CUR_TTY="/tmp/muxlog_info"
-elif [ "$CFW_NAME" == "TrimUI" ]; then
-  CUR_TTY="/dev/fd/1"
-else
-  CUR_TTY="/dev/tty0"
-fi
+# Set variables
+GAMEDIR="$directory/ports/soh"
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+cd $GAMEDIR
 
 # Exports
-export LD_LIBRARY_PATH="$GAMEDIR/libs:/usr/lib"
+export LD_LIBRARY_PATH="$GAMEDIR/libs:/usr/lib":$LD_LIBRARY_PATH
+export SDL_GAMECONTROLLERCONFIG=$sdl_controllerconfig
 
 # Permissions
 $ESUDO chmod 666 /dev/tty0
@@ -41,32 +35,37 @@ $ESUDO chmod 666 /dev/tty1
 $ESUDO chmod 777 $GAMEDIR/assets/extractor/otrgen.txt
 $ESUDO chmod 777 $GAMEDIR/assets/extractor/ZAPD.out
 
-cd $GAMEDIR
-
 # List of compatibility firmwares
-CFW_NAMES="ArkOS|ArkOS wuMMLe|ArkOS AeUX|knulli|TrimUI"
+CFW_NAMES="ArkOS:ArkOS wuMMLe:ArkOS AeUX:knulli:TrimUI"
 
 # Check if the current CFW name is in the list
 contains() {
-    local value="$1"
+    local value="$CFW_NAME"
     local item
-    IFS="|" # Use | as the delimiter
+    local tmp=$IFS
+    IFS=":" # Use : as the delimiter
+    echo "Checking if CFW_NAME '$value' is in the list..."
     for item in $CFW_NAMES; do
+        echo "Comparing '$item' with '$value'..."
         if [ "$item" = "$value" ]; then
+            echo "Match found: '$item'"
+            IFS=$tmp
             return 0
         fi
     done
+    echo "No match found for '$value'."
+    IFS=$tmp
     return 1
 }
 
 # If it's in the list use the compatibility binary
 if contains "$CFW_NAME" $CFW_NAMES; then
-    cp -f "$GAMEDIR/bin/compatibility.elf" soh.elf
+    cp -f "$GAMEDIR/bin/compatibility.elf" "$GAMEDIR/soh.elf"
     if [ "$(find ./mods -name '*.otr')" ]; then
         echo "WARNING: .OTR MODS FOUND! PERFORMANCE WILL BE LOW IF ENABLED!!" > $CUR_TTY
     fi
 else
-    cp -f "$GAMEDIR/bin/performance.elf" soh.elf
+    cp -f "$GAMEDIR/bin/performance.elf" "$GAMEDIR/soh.elf"
 fi
 
 # Check if we need to generate any otr files
@@ -88,7 +87,6 @@ fi
 $ESUDO chmod 777 $GAMEDIR/soh.elf
 echo "Loading, please wait... (might take a while!)" > $CUR_TTY
 $GPTOKEYB "soh.elf" -c "soh.gptk" & 
-SDL_GAMECONTROLLERCONFIG=$sdl_controllerconfig
 ./soh.elf
 
 # Cleanup
