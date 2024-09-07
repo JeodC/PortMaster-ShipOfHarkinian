537baec41206c8f5fc35f4fdfdc0cbe498116c2d
JeodC <47716344+JeodC@users.noreply.github.com>
2024-09-07 10:22:39 -0400

    Update

--- Ship of Harkinian.sh
+++ Ship of Harkinian.sh
@@ -20,7 +20,7 @@ source $controlfolder/device_info.txt
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 
 # Set variables
-GAMEDIR="$directory/ports/soh"
+GAMEDIR="/$directory/ports/soh"
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
 cd $GAMEDIR
